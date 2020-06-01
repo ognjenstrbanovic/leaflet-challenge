3f68a5063552c24b0b9f80adeac5472ac41ee222
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-05-31 22:32:18 -0400

    fixed API_KEY issues

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,1 +1,2 @@
-API_KEY = "pk.eyJ1Ijoib2duamVuc3RyYmFub3ZpYyIsImEiOiJja2FlaHg0N2wwZXh1MnlwbjdxN3E4cWZuIn0.ybK2LqphnYi2MLiwFMprPA"
+// API key
+const API_KEY = "pk.eyJ1Ijoib2duamVuc3RyYmFub3ZpYyIsImEiOiJja2FlaHg0N2wwZXh1MnlwbjdxN3E4cWZuIn0.ybK2LqphnYi2MLiwFMprPA"
