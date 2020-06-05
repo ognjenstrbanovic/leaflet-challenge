d4ad05372e06fbcb947f996803b30931acc42839
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-06-05 16:47:39 -0400

    tweaks/changes w/ TA help

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,18 +29,12 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // Define lightmap layer
-  var lightmap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: "mapbox://styles/mapbox/light-v11",
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: API_KEY,
-    }
-  ).addTo(myMap);
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXdhcmRlaCIsImEiOiJja2FkNGZ0OGgyMGtqMnlwbThnNXR3bWNpIn0.uFK4y-WgrYAmarpTNRzedg", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
